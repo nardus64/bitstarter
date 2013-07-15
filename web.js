35075e9fc82d0654ee5b95ba486c6a45c4761f05
nardus64 <nardus64@gmail.com>
2013-07-15 00:50:45 +0000

    fix web.js na edwin my

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 var dat = new Buffer(200,'utf8');
-//var err = new Buffer(50);
+var err = new Buffer(50,'utf8');
 var fs = require('fs');
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.txt', encoding='utf8',function(err,dat){
  if (err) { throw err;}
-  return (dat.toString);})
-);
+  return (dat.toString());});
 });
 
 var port = process.env.PORT || 5000;
