7af55d19d56ee1319b2df09464b5efd8acab68ab
nardus64 <nardus64@gmail.com>
2013-07-24 18:32:17 +0000

    last commit with video

--- web.js
+++ web.js
@@ -3,12 +3,14 @@ var app = express.createServer(express.logger());
 var dat = new Buffer(200,'utf8');
 var err = new Buffer(50,'utf8');
 var fs = require('fs');
+var filename = 'index.html'
 app.use("/static",express.static(__dirname+"/static"));
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', encoding='utf8',function(err,dat){
+ /*  response.send(fs.readFileSync('index.html', encoding='utf8',function(err,dat){
  if (err) { throw err;}
-  return (dat.toString());})
+  return (dat.toString());})*/
+response.send(fs.readFileSync(filename).toString()
 );
 });
 
